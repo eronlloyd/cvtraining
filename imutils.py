c479f75432868d909856dff4bfa6d04c11027f22
Eron Lloyd <eronlloyd@gmail.com>
2018-05-20 12:01:27 -0400

    Ch. 6 progress

---
+++ imutils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+def translate(image, x, y):
+    M = np.float32([[1, 0, x], [0, 1, y]])
+    shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+
+    return shifted
+
+def rotate(image, angle, center=None, scale=1.0):
+    (h, w) = image.shape[:2]
+
+    if center is None:
+        center = (w // 2, h //2)
+
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+
+    return rotated
